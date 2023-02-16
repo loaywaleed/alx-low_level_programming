d5b481f248255f8f92f0d54559b241c1a013fe9e
loaywaleed <loaywaleed9@gmail.com>
2023-02-16 12:35:37 -0800

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-/*Function that prints a statement with new line*/
+/**
+ * Function that prints a statement with new line
+ */
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
