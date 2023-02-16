ecd3ff8b9198597ff043795bca0e1491e247e5dc
loaywaleed <loaywaleed9@gmail.com>
2023-02-16 13:06:26 -0800

    sizeof operator

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ * Return - 0 for success
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %ul byte(s)", sizeof(char));
+	printf("Size of an int: %ul byte(s)", sizeof(int));
+	printf("Size of a long int: %ul byte(s)", sizeof(long int));
+	printf("Size of a long long int: %ul byte(s)", sizeof(long long int));
+	printf("Size of a float: %ul byte(s)", sizeof(float);
+	return (0);
+}
