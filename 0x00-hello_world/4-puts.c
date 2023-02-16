821c27fcc325f36888e81242285625ee43e31644
loaywaleed <loaywaleed9@gmail.com>
2023-02-16 12:39:01 -0800

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 /**
- * Function that prints a statement with new line
+ * main - Point of entry
+ * Return: Returns 0 to signal success
  */
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
