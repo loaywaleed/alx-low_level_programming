edc9d994fd0a3206ef0f35cb0addd4de39f33dbd
loaywaleed <loaywaleed9@gmail.com>
2023-02-16 12:43:26 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main: Entry point
+ * Return: returns 0 to signal success
+ */
+#include <stdio.h>
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
