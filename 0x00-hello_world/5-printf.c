e0a0371492248124edec92c74de41951fabc4a58
loaywaleed <loaywaleed9@gmail.com>
2023-02-16 12:44:54 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * Return: returns 0 to signal success
  */
 #include <stdio.h>
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
