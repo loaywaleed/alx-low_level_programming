f6a5b60d5d368ab77b80115d864460f0fef5a4a0
loaywaleed <loaywaleed9@gmail.com>
2023-02-17 12:58:23 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		while(!(i == 'q' || i == 'e'))
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
