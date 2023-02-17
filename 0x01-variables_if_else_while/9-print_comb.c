72e69b38a44c63432598c909ac67ad6a09c17582
loaywaleed <loaywaleed9@gmail.com>
2023-02-17 13:48:00 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48;i <= 57; i++)
+	{
+		putchar(i);
+		while (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
