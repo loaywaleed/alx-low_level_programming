79f5159e71824ef5f54f8be308d5f1d9bedb52d3
loaywaleed <loaywaleed9@gmail.com>
2023-02-21 15:25:43 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - starting point of the program
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,8 @@
  *
  * Return: On success 1
  * 0n error, -1 is returned, and errno is set appropriately
+ * print alphabet
  */
 
 int _putchar(char c);
+void print_alphabet(void);
