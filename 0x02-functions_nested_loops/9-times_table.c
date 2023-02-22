11014b4ca9838c10eabe501d49e6a4c6d31aac96
loaywaleed <loaywaleed9@gmail.com>
2023-02-21 17:56:52 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table - 9 times table
+ *  Return: void
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0, j < 10; j++)
+		{
+			result = i * j;
+			_putchar('0' + result);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
