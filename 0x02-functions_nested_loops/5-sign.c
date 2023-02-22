f594412ca89cb6ad80eabe72726ece6136614d53
loaywaleed <loaywaleed9@gmail.com>
2023-02-21 16:34:47 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - prints whether a number is positive or negative
+ * Return: 0 if zero, 1 if positive, -1 if negative
+ * @n: number to be checked
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
