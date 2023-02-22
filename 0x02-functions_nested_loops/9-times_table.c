46109dd093c4b36af7907038a88c840c290d4938
loaywaleed <loaywaleed9@gmail.com>
2023-02-21 18:24:44 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,22 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			result = i * j;
-			if (result < 10 && result >= 0)
+			if (result == 0)
 			{
+				putchar(k + '0');
+			}
+			if (result < 10 && result > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + result);
+			} else 
+			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-			} else {
-				_putchar('0' + result/10);
-				_putchar('0' + result%10);
-				_putchar(',');
-				_putchar(' ');
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
 			}
 		}
 		_putchar('\n');
