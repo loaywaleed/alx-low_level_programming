d2b12c17dacedc55dd6242573f109d0524e08cbc
loaywaleed <loaywaleed9@gmail.com>
2023-02-21 17:38:23 -0800

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,11 @@ void jack_bauer(void)
 	{
 		for (mins = 0; mins < 60; mins++)
 		{
-			_putchar('0' + hours / 10);
-			_putchar('0' + hours % 10);
-			_putchar('0' + ':');
-			_putchar('0' + mins / 10);
-			_putchar('0' + mins % 10);
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (mins / 10));
+			_putchar('0' + (mins % 10));
 		}
 	}	
 }
