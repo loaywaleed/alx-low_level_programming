6c25aae1e24d5d95d6f6625d25f9b097b4bc2d91
loaywaleed <loaywaleed9@gmail.com>
2023-02-21 16:24:15 -0800

    is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * isalpha - checks if a number is alphabet
+ * _isalpha - checks if a number is alphabet
  * Return: 1 if leteer, 0 otherwise
  * @c: argument to be checked
  */
 
 int _isalpha(int c)
 {
-	return ((c > 97 && c <= 122)||(c > 65 && c <= 90));
+	return ((c > 97 && c <= 122) || (c > 65 && c <= 90));
 }
-
-
