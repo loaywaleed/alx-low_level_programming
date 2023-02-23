81cbd6d7dd78b6c68209f8705a5d23cccd66a02a
loaywaleed <loaywaleed9@gmail.com>
2023-02-23 10:29:18 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * print_line - prints a straight line
+ * Return: void
+ * @n: length of the straight line
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
