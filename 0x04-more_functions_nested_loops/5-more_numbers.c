880bf185b93f41a4a96a643ec03d6061fdbf975c
loaywaleed <loaywaleed9@gmail.com>
2023-02-23 09:49:43 -0800

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints numbers from 0-14 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	while (i = 0; i < 10; i++)
+	{
+		while (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0')
+		}
+		_putchar('\n');
+	}
+}
