ac78efdb074e2d3317a74661689d39a51e9c1b18
loaywaleed <loaywaleed9@gmail.com>
2023-02-23 13:37:27 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * Return: product of two numbers
+ * @a: first num
+ * @b: second num
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
