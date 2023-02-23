466bf8919710841e08c917328e16aaf4a90e95c2
loaywaleed <loaywaleed9@gmail.com>
2023-02-23 13:17:16 -0800

    hello

---
+++ 0x04-more_functions_nested_loops/1-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - checks uppercase
+ * Return: 1 or 0
+ * @c: takes in integer
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
