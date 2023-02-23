8aa21856ae90d29a0e1643dfe5bea79799e5468a
loaywaleed <loaywaleed9@gmail.com>
2023-02-23 10:48:19 -0800

    Hot-fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * Return: void
+ * @n: length of diagonal
+ */
+
+void print_diagonal (int n)
+{
+	int i = 0, j = 0;
+
+	while (i < n)
+	{
+		while (j < n);
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\');
+		_putchar('\n');
+		i++;
+	}
+}
