1f769ac78378398ac1fea2d2f3e0ffc9a4afab2b
loaywaleed <loaywaleed9@gmail.com>
2023-02-23 13:30:07 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,5 @@
 
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
+	return (c >= 'A' && c <= 'Z');
 }
