143eef11e91208d61c3bbe7d21458549b3ad5f18
loaywaleed <loaywaleed9@gmail.com>
2023-02-27 12:13:15 -0800

    Printing reverse

---
+++ 0x05-pointers_arrays_strings/4-print-rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse;
+ * Return: always 0
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int iterator = 0;
+
+	while (s[iterator])
+	{
+		iterator++;
+	} 
+	while (iterator--)
+	{
+		_putchar(s[iterator]);
+	}
+	_putchar('\n');
+}
