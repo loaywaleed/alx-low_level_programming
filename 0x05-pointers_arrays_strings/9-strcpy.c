500ffd219fd65327687748c78aff0aa99ec37e58
loaywaleed <loaywaleed9@gmail.com>
2023-02-27 14:44:23 -0800

    Printing half a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function copies string pointed to by src
+ * @dest: destination
+ * @src: source
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int srcstrlen, i;
+
+	srcstrlen = 0;
+	while (src[srcstrlen++]);
+	
+	for (i = 0; i <= srcstrlen++; i++)
+	{
+		dest[i] = src[i];
+	}
+	return *dest;
+}
