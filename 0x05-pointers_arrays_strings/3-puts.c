63a2bd7a3c5018e69d8d4272a3d054da54ec884b
loaywaleed <loaywaleed9@gmail.com>
2023-02-27 11:45:14 -0800

    Puts Function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * Return: void
+ * @str: input string
+ */
+
+void _puts(char *str)
+{
+	while (str[i])
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
