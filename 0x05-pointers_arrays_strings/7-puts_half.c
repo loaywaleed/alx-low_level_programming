62410c5ff9d832670452bf97e79b61a840c4843d
loaywaleed <loaywaleed9@gmail.com>
2023-02-27 14:00:40 -0800

    Printing half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a sting
+ * Return: void
+ * @str: input string
+ */
+
+void puts_half(char *str)
+{
+	int i, strlen, j;
+
+	while (str[i])
+	{
+		strlen++;
+	}
+	if (strlen % 2 == 0)
+	{
+		starting_point = strlen / 2;
+	}
+	else
+	{
+		starting_point = (strlen - 1) / 2;
+	}
+	for (j = starting_point; j < strlen; j++)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
