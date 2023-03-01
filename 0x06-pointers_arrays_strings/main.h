397cc5f850265a14f4e5191c72f2650ef4bbec55
loaywaleed <loaywaleed9@gmail.com>
2023-03-01 11:14:23 -0800

    concat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,9 +6,10 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, in n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+void reverse_array(int *a, int n);
+char *string_toupper(char *n);
+char *cap_string(char *n);
+char *leet(char *n);
+char *rot13(char *n);
 
 #endif /* MAIN_H */
