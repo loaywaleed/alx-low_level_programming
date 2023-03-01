60c6de0e9693d78d5238ebe61e070a3997ecf440
loaywaleed <loaywaleed9@gmail.com>
2023-03-01 12:50:24 -0800

    copying strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * Return: char
+ * @dest: destination string
+ * @src: source string
+ * @n: size
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[j] = src[j];
+	}
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	if (dest[i-1] != '\0')
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
