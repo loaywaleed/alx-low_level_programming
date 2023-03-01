1f78026d5e89c984d0e5887cb5722ab8b49095f4
loaywaleed <loaywaleed9@gmail.com>
2023-03-01 14:58:42 -0800

    lowercase to upper

---
+++ 0x06-pointers_arrays_strings/5-string_to_upper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_to_upper - converts string lowercase to uppercase
+ * Return: pointer to char
+ * @c: string
+ */
+
+char *string_to_upper(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		if (c[i] >= 'a' &&  c[i] <= 'z')
+		{
+			c[i] -= 32;
+		}
+		i++;
+	}
+	return (c);
+}
