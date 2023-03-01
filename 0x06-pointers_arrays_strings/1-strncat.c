f5c5c5cb1c8addfe52dbed68dd4a8994c0d25117
loaywaleed <loaywaleed9@gmail.com>
2023-03-01 11:32:27 -0800

    concatenation of strings with size

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - appending a number of characters
+ * Return: char
+ * @dest: destination parameter
+ * @src: source parameter
+ * @n: numbers
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = src[j]
+		if (src[j] == '\0')
+		{
+			break;
+		}
+	}
+
+	return (dest);
+}
