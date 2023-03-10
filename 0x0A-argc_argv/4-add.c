1d1aafa5649fd38afa2fafe43f2e2230eac5c021
loaywaleed <loaywaleed9@gmail.com>
2023-03-10 13:18:37 -0800

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		i = 1;
 		while (i < argc)
 		{
-			if (*argv[i] > '0' && *argv[i] < '9')
+			if (*argv[i] >= '0' && *argv[i] <= '9')
 			{
 				i++;
 				sum += atoi(argv[i]);
