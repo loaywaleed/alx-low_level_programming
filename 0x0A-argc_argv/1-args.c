0c64c7a5b303c7b94c04b975ac2f4e8e9ee12a26
loaywaleed <loaywaleed9@gmail.com>
2023-03-10 12:03:09 -0800

    printing number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of terminal arguments
+ * @argv: array of arguments
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
