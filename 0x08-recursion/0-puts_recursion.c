7c0c91299ee35c524d3a0209798f97e49236e20a
loaywaleed <loaywaleed9@gmail.com>
2023-03-11 11:13:10 -0800

    Puts function in recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts function in recursion
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s = '\0')
+	{
+		_putchar(0);
+	}
+	_putchar(s[0]);
+	return (_puts_recursion(s+1));
+}
