a3ebc441bcad4fa469e6e26a382e378d0e11f566
loaywaleed <loaywaleed9@gmail.com>
2023-03-14 13:34:43 -0700

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int len1, len2, i, j;
 
-	while(s1[len1])
+	while(s1 && s1[len1])
 	{
 		len1++;
 	}
-	while (s2[len2])
+	while (s2 && s2[len2])
 	{
 		len2++;
 	}
@@ -39,7 +39,7 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	if (s2)
 	{
-		while(i < (len1 + len2))
+		while (i < (len1 + len2))
 		{
 			a[i] = s2[j];
 			i++;
