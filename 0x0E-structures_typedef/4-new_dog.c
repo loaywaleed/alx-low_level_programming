99d19eda7e8309c5e0f1880c258258ab4f4207f6
loaywaleed <loaywaleed9@gmail.com>
2023-03-20 13:55:15 -0700

    copying to a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k;
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	i = 0, j = 0;
+	while(name[i])
+	{
+		i++;
+	}
+	while(owner[j])
+	{
+		j++;
+	}
+	p->name = malloc(i + 1);
+	p->owner = malloc(j + 1);
+
+	if (p->name == NULL || p->owner == NULL)
+	{
+		free(p->name), free(p->owner), free(p);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		p->name[k] = name[k];
+	}
+	p->owner[k] = '\0';
+	p->age = age;
+	return (0);
+
+}
