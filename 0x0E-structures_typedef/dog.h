a462410866ce36e32de7a24836499fcca2f32a75
loaywaleed <loaywaleed9@gmail.com>
2023-03-20 13:16:41 -0700

    Print dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,14 @@
  * @owner: dog's owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 
 #endif
