bd4c5538a42e6b11aeea333400a4f6698cabe497
loaywaleed <loaywaleed9@gmail.com>
2023-03-22 16:10:21 -0700

    Hot-fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,8 +18,8 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 	if ((argv[2][1] != 0) || ((argv[2][0] != '+') && (argv[2][0] != '-')
-			       	&& (argv[2][0] != '*') && (argv[2][0] != '/') &&
-			       	(argv[2][0] != '%')))
+		&& (argv[2][0] != '*') && (argv[2][0] != '/') &&
+		(argv[2][0] != '%')))
 	{
 		puts("Error");
 		exit(99);
