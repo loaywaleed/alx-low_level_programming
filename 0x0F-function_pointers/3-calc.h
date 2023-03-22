553224629a1afd428b9c3695cc873f35fa8f60d6
loaywaleed <loaywaleed9@gmail.com>
2023-03-22 16:14:30 -0700

    Added documentation for struct

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -8,10 +8,15 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+/**
+ * struct op - struct that selects operations
+ * @op: operation symbol
+ * @f: function to perform
+ */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 #endif
