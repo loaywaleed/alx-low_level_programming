082145f3474119e2695d1123f34c9eb1527598da
loaywaleed <loaywaleed9@gmail.com>
2023-03-23 14:22:29 -0700

    Finding sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - function that sums all of its parameters
+ * @n: first arg
+ * Return: Result of sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i;
+	int sum = 0;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, const unsigned int);
+	}
+	va_end(args);
+	return (sum);
+}
