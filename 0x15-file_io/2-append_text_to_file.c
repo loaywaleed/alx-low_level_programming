f9ca8f9d456d3883000bfb642946809ba6cc4ba7
loaywaleed <loaywaleed9@gmail.com>
2023-04-10 15:36:03 -0700

    Created a function that appends text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * create_file - funtion that creates a file
+ * append_text_to_file - funtion that creates a file
  * @filename: name of the file
  * @text_content: content to be written in the file
  * Return: 1 on succes, -1 on failure
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int letters = 0;
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | 0_APPEND);
 	if (fd == -1)
 	{
 		return (fd);
