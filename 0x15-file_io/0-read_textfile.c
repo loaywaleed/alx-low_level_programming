b4bd7e34a1ee6a0874c70cac50d5d6c1cdfbffdd
loaywaleed <loaywaleed9@gmail.com>
2023-04-10 14:49:16 -0700

    Reading a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text from a file
+ * @filename: name of the file
+ * @letters: letters to be read
+ * Return: number of letters read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	int x;
+	int file_to_write_in;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buf = malloc(letters);
+	x = read(fd, buf, letters);
+	if (x == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[x] = '\0';
+	close(fd);
+	file_to_write_in = write(STDOUT_FILENO, buf, letters);
+	if (file_to_write_in == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (file_to_write_in);
+}
