9b663d70e091cb0793c71dbb0738adad06569489
loaywaleed <loaywaleed9@gmail.com>
2023-04-10 16:46:34 -0700

    Setting a bit value

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - setting a bit to 1;
+ * @n: pointer to number
+ * @index: bit to be set
+ * Return: 1 for success, -1 for error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if ((index > sizeof(long int) * 8 - 1) || n == NULL)
+	{
+		return (-1)
+	}
+	*n |= (1 << index);
+	return (1);
+}
