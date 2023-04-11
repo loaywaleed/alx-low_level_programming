9610e73637ef0d29c12321133ae3711b9b3a3499
loaywaleed <loaywaleed9@gmail.com>
2023-04-10 18:19:34 -0700

    endianness and cracking password

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks endianness
+ * Return: 0 for big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 0;
+	char *c = (char*) &i;
+	
+	return ((int)*c);
+}
