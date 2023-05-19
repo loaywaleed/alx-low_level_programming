8b9434fcc7003302275b527768a840b7d4df8de9
Loay Waleed <loaywaleed9@gmail.com>
2023-05-19 12:15:28 -0700

    Hot fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,12 +3,13 @@
  * add_dnodeint - function that adds new node at the beginning
  * @head: head pointer
  * @n: data to be entered
+ * Return: new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	new_node =malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
 		return (NULL);
