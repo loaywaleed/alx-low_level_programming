26c6fa8cef896be326f7f676d8bb7bbc3326f972
Loay Waleed <loaywaleed9@gmail.com>
2023-05-19 00:50:50 -0700

    Hot fix

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,7 +17,7 @@ typedef struct dlistint_s
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
-} dlistint_t
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
