b160de879719ae990344a374649d190da1b63111
root <loaywaleed9@gmail.com>
2023-07-07 11:08:03 -0700

    Hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
-	hash_table = malloc(sizeof(hash_table));
+	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
@@ -23,7 +23,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
+	{
 		hash_table->array[i] = NULL;
-
-	return (hash_table);	
+	}
+	return (hash_table);
 }
