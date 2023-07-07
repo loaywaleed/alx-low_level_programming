d4064f82d701e9af49017d621eb5c4f90f6730ba
root <loaywaleed9@gmail.com>
2023-07-07 11:22:31 -0700

    Creating a hash function

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Hash function that returns index of a key
+ * @key: Key entered
+ * @size: Size of the hash_table
+ * Return: Index of the key entered
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key) % size;
+	return (index);
+}
